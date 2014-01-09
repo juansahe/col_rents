f0bab36e726fff0b8481158086d84e67cee7df8c
Juan Sanchez <juansahe@gmail.com>
2014-01-09 11:48:02 -0500

    Locales Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	before_action :set_locale
+	# Prevent CSRF attacks by raising an exception.
+  	# For APIs, you may want to use :null_session instead.
+  	protect_from_forgery with: :exception
+
+  	private
+    def set_locale
+      I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+    end
 end

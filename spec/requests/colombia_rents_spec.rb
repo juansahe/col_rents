f43c8b656e5474d973a7a23416a424a95a2cdae8
Juan Sanchez <juansahe@gmail.com>
2014-01-14 11:13:49 -0500

    Pulled

--- spec/requests/colombia_rents_spec.rb
+++ spec/requests/colombia_rents_spec.rb
@@ -4,29 +4,37 @@ describe "Colombia Rents" do
 
   subject { page }
 
+  shared_examples_for "all colombia rents pages" do
+    it { should have_selector('a', text: linkeage ) }
+    it { should have_selector('h1', text: heading ) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Main" do
     before { visit root_path }
-    
-    it { should have_content('Colombia Rents') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Inicio') }
+    let(:linkeage)      { 'Colombia Rents' }
+    let(:heading)       { '' }
+    let(:page_title)    { '' }
+
+    it_should_behave_like "all colombia rents pages"
+    it { should_not have_title('| Inicio')}
 
   end
 
   describe "About" do
     before { visit about_path }
   	
-  	it { should have_content('Quienes Somos') }
-    it { should have_title('Quienes Somos') }
+  	let(:heading)        { 'Quienes Somos' }
+    let(:page_title)     { 'Quienes Somos' }
 
   end
 
   describe "Contact" do
     before { visit contact_path }
   	
-    it { should have_content('Contacto') }
+    let(:heading)     { 'Contacto' }
+    let(:page_title)  { 'Contacto'}
     it { should have_title('Contacto') }
 
   end
-
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,1 @@
-# Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "Colombia Rents | Alquiler de Inmuebles en Cartagena Colombia"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
+include ApplicationHelper

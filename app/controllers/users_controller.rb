ebfe4717b8cb046578784dc9e461dedf261fafcd
Juan Sanchez <juansahe@gmail.com>
2014-01-15 19:26:56 -0500

    WIP Modal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def new
   	@user = User.new
+  	respond_to do |format|
+      format.html { render('shared/signmodal') }
+    end
   end
   
   def show

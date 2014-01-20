457ba2ca591dcdae82ca55f984527c023e269a74
Juan D Sanchez <juansahe@gmail.com>
2014-01-20 07:41:55 -0500

    Navbar

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,5 +8,26 @@ describe "Authentication" do
 
 		it { should have_title('Sign In') }
 		it { should have_content('Sign In') }
+    
+    describe "with invalid credentials" do
+      before { click_button "Log In" }
+      
+      it { should have_title('Log In') }
+      it { should have_selector('div.alert.alert-error') }
+    end
+    
+    describe "with valid credentials" do
+      let(:user_auth) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",      with: user_auth.email.upcase
+        fill_in "Password",   with: user_auth.password
+        click_button "Log In"
+      end
+      
+      it { should have_title(user_auth.name) }
+      it { should have_link('Profile',    href: user_path(user)) }
+      it { should have_link('Log Out',    href: signout_path) }
+      it { should_not have_link('Log In', href: signin_path) }
+    end
 	end
 end
